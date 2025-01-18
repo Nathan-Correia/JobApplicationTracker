462f682060f64a497d303291a99d5b4cb788ba59
Cameron Jubinville <jubinvillecam@gmail.com>
2025-01-17 23:53:49 -0700

    Added deleting + adding weird data, need to make a nice thing afterwards but its a start

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,13 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_landing, name='dashboard'),
-    path('jobs/', views.job_list, name='job_list'),
-    path('jobs/<int:pk>/', views.job_detail_partial, name='job_detail_partial'),
-    path('jobs/create/', views.job_create, name='job_create'),
-    path('jobs/<int:pk>/edit/', views.job_edit, name='job_edit'),
-    path('jobs/<int:job_pk>/communication/create/', views.communication_create, name='communication_create'),
-    path('resumes/', views.resume_list, name='resume_list'),
-    path('resumes/<int:pk>/', views.resume_detail, name='resume_detail'),
-    path('resumes/create/', views.resume_create, name='resume_create'),
+    path('', views.main_landing, name='main_landing'),
+    path('add_job_list', views.add_job_list, name='add_job_list'),
+    path('refresh_job_list', views.refresh_job_list, name='refresh_job_list'),
+    path('delete/', views.delete_individual, name='delete_individual'),
 ]
